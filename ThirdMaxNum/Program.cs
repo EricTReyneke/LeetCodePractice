b9ef7ad16bb1cb23ac4988eda81d8a147f73bf21
Eric Theodore Reyneke <91662607+EricTReyneke@users.noreply.github.com>
2023-02-22 21:33:55 +0200

    Add files via upload

---
+++ ThirdMaxNum/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ThirdMaxNum
+{
+    class Program{
+        public static void Main(){
+            int[] numArr = new int[]{2,2,3,1};
+            Console.WriteLine(FetchThirdMax(numArr));
+
+            Console.ReadKey();
+        }
+
+        public static int FetchThirdMax(int[] nums){
+            HashSet<int> numsHash = nums.ToHashSet();
+            List<int> numsList = numsHash.ToList();
+            int len = numsList.Count;
+            int counter = 0;
+
+            if (len > 2)
+            {
+                while (counter < 2)
+                {
+                    numsList.Remove(numsList.Max());
+                    counter++;
+                }
+            }
+            else
+            {
+                return numsList.Max();
+            }
+
+            return numsList.Max();
+        }
+    }
+}
